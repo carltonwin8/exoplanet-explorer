d4eee8aeb890892e597ef4404b0fc6c4661b5846
Carlton Joseph <carlton.joseph@gmail.com>
2017-08-03 22:33:32 -0700

    add map to promomis

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,15 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then(function (data) {
+        console.log(data);
+        addSearchHeader(data.query);
+        data.results.map(function (e) {
+          console.log(e);
+          getJSON(e)
+            .then(createPlanetThumb);
+        });
+      });
   });
 })(document);
